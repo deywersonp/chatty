97109b95d21a6d8ea7acb181cb20b92e68711e7c
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-04-23 18:36:14 -0300

    create messages - table, entity, service & controller; add messages on routes.ts

---
+++ src/controllers/MessagesController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { MessagesService } from '../services/MessagesServices';
+
+
+class MessagesController {
+
+  async create(request: Request, response: Response) {
+    const { admin_id, text, user_id } = request.body;
+    const messageService = new MessagesService();
+
+    const message = await messageService.create({
+      admin_id,
+      text,
+      user_id,
+    })
+
+    return response.json(message);
+  }
+}
+
+export { MessagesController }
