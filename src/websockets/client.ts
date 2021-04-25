328a1fcf221ecd371a5f851b7e3db6c062407549
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-04-25 07:18:38 -0300

    merge conflit

--- src/websockets/client.ts
+++ src/websockets/client.ts
@@ -47,10 +47,6 @@ io.on("connect", (socket) => {
     await messagesService.create({
       text,
       user_id
-    });
-
-    const allMessages = await messagesService.listByUser(user_id);
-
-    socket.emit("client_list_all_messages", allMessages);
+    })
   });
 });
